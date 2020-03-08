c70f8b2cce82681f20be939d8e49e8da4c368f6c
Matthew Alvarez <malvar29@ucsc.edu>
2020-03-08 13:34:00 -0700

    added method to get active chef contestants

--- Assets/genAlg.cs
+++ Assets/genAlg.cs
@@ -2,8 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class genAlg : MonoBehaviour
 {
+
+    //Declare List of all contestants
+    public List<chef> contestants;
+
+
+    //Gets all active contenstats in the game
+    //returns list of chefs
+    public List<chef> currentContestants()
+    {
+        contestants = new List<chef>(GameObject.FindGameObjectsWithTag("Respawn"));
+        return contestants;
+    }
+
+    
+
     // Start is called before the first frame update
     void Start()
     {
