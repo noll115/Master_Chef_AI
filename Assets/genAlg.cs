42298556699cff5d088683755a649b77432c448d
Matthew Alvarez <malvar29@ucsc.edu>
2020-03-10 17:39:57 -0700

    Changes to make file work with ChefRooms rather than Chefs

--- Assets/genAlg.cs
+++ Assets/genAlg.cs
@@ -4,8 +4,15 @@ using UnityEngine;
 using System;
 
 
-public class genAlg : MonoBehaviour
+public static class genAlg : MonoBehaviour
 {
+
+    /*generate chefRooms
+    /
+    /
+    /
+    *\
+
     /*
     /crossover
     /Changes all chef stats based on the last round.
@@ -13,17 +20,17 @@ public class genAlg : MonoBehaviour
     /   Args: 
     /       chef Winner - The winner of the last round
     */
-    private void crossover(Chef Winner, Dictionary<uint, Chef>Chefs)
+    private void crossover(Chef Winner, Dictionary<uint, ChefRoom> ChefRooms)
     {
         //for each chef in the game
-        foreach(KeyValuePair<uint, Chef>chef in Chefs)
+        foreach(KeyValuePair<uint, ChefRoom> room in ChefRooms)
         {
             //operate on stats
-            chef.Value.stove = chef.Value.stove/2 + Winner.stove/2;
-            chef.Value.oven = chef.Value.oven/2 + Winner.oven/2;
-            chef.Value.cutting = chef.Value.cutting/2 + Winner.cutting/2;
-            chef.Value.stirring = chef.Value.stirring/2 + Winner.stirring/2;
-            chef.Value.plating = chef.Value.plating/2 + Winner.plating/2;
+            room.Value.Chef.stove = room.Value.Chef.stove/2 + Winner.stove/2;
+            room.Value.Chef.oven = room.Value.Chef.oven/2 + Winner.oven/2;
+            room.Value.Chef.cutting = room.Value.Chef.cutting/2 + Winner.cutting/2;
+            room.Value.Chef.stirring = room.Value.Chef.stirring/2 + Winner.stirring/2;
+            room.Value.Chef.plating = room.Value.Chef.plating/2 + Winner.plating/2;
         }
     }
 
@@ -31,19 +38,19 @@ public class genAlg : MonoBehaviour
     /mutation
     /Random chancce to change chef stats that cannot be learned.
     */
-    private void mutation(Dictionary<uint, Chef>Chefs)
+    private void mutation(Dictionary<uint, ChefRoom> ChefRooms)
     {
-        foreach(KeyValuePair<uint, Chef>chef in Chefs)
+        foreach(KeyValuePair<uint, ChefRoom> rooms in ChefRooms)
         {
             //random number to see if mutation or not
             double random = UnityEngine.Random.value;
 
             //If random < 0.3 get less confident
             if (random <= 0.3)
-                chef.Value.confidence = chef.Value.confidence*0.75;
+                room.Value.Chef.confidence = room.Value.Chef.confidence*0.75;
             //if random >0.7 get more confident
             else if (random >= 0.7)
-                chef.Value.confidence = Math.Min(1, chef.Value.confidence*1.25);
+                room.Value.Chef.confidence = Math.Min(1, room.Value.Chef.confidence*1.25);
 
             //if random is not either, no change
         }
@@ -52,14 +59,16 @@ public class genAlg : MonoBehaviour
     /*
     /geneticAlg
     /Genetic algorithm implementation. Develops chef skills in between rounds.
-    /   Args: 
-    /       chef Winner - The winner of the last round
+    /   Args:
+    /       uint[] Winners - The key for all winners of last round.
+    /       Dictionary<uint, ChefRoom> ChefRooms - Dictionary of all Chef rooms active.
     */
-    public void geneticAlg(Chef Winner, Dictionary<uint, Chef>Chefs)
+    public static void geneticAlg(uint[]Winners, Dictionary<uint, ChefRoom> ChefRooms)
     {
+
         //operate on contestants
-        crossover(Winner, Chefs);
-        mutation(Chefs);
+        crossover(Winner, ChefRooms);
+        mutation(ChefRooms);
     }
 
     // Start is called before the first frame update
