5173f733610b22dd8c72136e6420592882c74700
ihorning <50390805+ihorning@users.noreply.github.com>
2020-03-11 22:07:12 -0700

    Chef's choice
    The chefs can choose what they want to add for some dishes. This is mostly random right now, but I will either make it so each choice of thing to add is its own action or a mini plan is made to determine which choice is the best.
    Added some new recipes and a meals list.

--- Assets/ActionPlanning.cs
+++ Assets/ActionPlanning.cs
@@ -37,7 +37,7 @@ public class ActionPlanning : MonoBehaviour
             Debug.Log(plan[i]);
         }*/
 
-        Debug.Log("Making a double cheesburger:");
+        /*Debug.Log("Making a double cheesburger:");
         initial = new State();
         initial["patty_raw"] = 2;
         initial["burger_bun"] = 2;
@@ -49,6 +49,35 @@ public class ActionPlanning : MonoBehaviour
         initial["mustard_bottle"] = 1;
         goal = new State();
         goal["doubleCheeseburger"] = 1;
+        plan = MakePlan(initial, goal);
+        for(int i = 0; i < plan.Count; i++) {
+            Debug.Log(plan[i]);
+        }*/
+
+        Debug.Log("Making a pizza:");
+        initial = new State();
+        initial["pizza_crust"] = 1;
+        initial["cheese"] = 2;
+        initial["tomato_slices"] = 1;
+        initial["sausage_cut"] = 1;
+        initial["pepper_green"] = 1;
+        initial["pepper_red"] = 1;
+        initial["bacon_raw"] = 1;
+        initial["bacon_cooked"] = 1;
+        goal = new State();
+        goal["pizza_cooked"] = 1;
+        plan = MakePlan(initial, goal);
+        for(int i = 0; i < plan.Count; i++) {
+            Debug.Log(plan[i]);
+        }
+
+        Debug.Log("Making large soup:");
+        initial = new State();
+        foreach(string ingredient in Categories["#soupIngredient"].Keys) {
+            initial[ingredient] = 3;
+        }
+        goal = new State();
+        goal["soup_large_cooked"] = 1;
         plan = MakePlan(initial, goal);
         for(int i = 0; i < plan.Count; i++) {
             Debug.Log(plan[i]);
@@ -90,15 +119,18 @@ public class ActionPlanning : MonoBehaviour
         // The action that turned the last state into this state
         Dictionary<State, Action> lastAction = new Dictionary<State, Action>();
         // The cost to get here
-        Dictionary<State, int> cost = new Dictionary<State, int>();
+        Dictionary<State, float> cost = new Dictionary<State, float>();
+        // The score so far
+        Dictionary<State, float> score = new Dictionary<State, float>();
 
         queue.Enqueue(0, new State(initialState));
         last.Add(initialState, null);
         lastAction.Add(initialState, null);
-        cost.Add(initialState, 0);
+        cost.Add(initialState, 0f);
+        score.Add(initialState, 0f);
 
         for(int i = 0; i < TRIES; i++) {
-            Debug.Log(queue);
+            //Debug.Log(queue);
             State currentState = queue.Dequeue();
             if(currentState == null) {
                 Debug.Log("No plan found");
@@ -112,13 +144,13 @@ public class ActionPlanning : MonoBehaviour
                     currentState = last[currentState];
                 }
                 plan.Reverse();
-                Debug.Log(plan);
+                //Debug.Log(plan);
                 return plan;
             } else {
                 // Continue searching
                 Dictionary<Action, State> options = GetActions(currentState);
                 foreach(Action action in options.Keys) {
-                    int newCost = cost[currentState] + 1; // Replace the 1 with action cost later when actions get costs
+                    float newCost = cost[currentState] + 1; // Replace the 1 with action cost later when actions get costs
                     if(!(cost.ContainsKey(options[action])) || (cost[options[action]] > newCost)) {
                         last.Add(options[action], currentState);
                         lastAction.Add(options[action], action);
@@ -126,7 +158,7 @@ public class ActionPlanning : MonoBehaviour
                         queue.Enqueue(newCost, options[action]);
 
                         foreach(State key in cost.Keys) {
-                            Debug.Log("COST "+key);
+                            //Debug.Log("COST "+key);
                         }
                     }
                 }
@@ -155,14 +187,23 @@ public class ActionPlanning : MonoBehaviour
         Dictionary<Action, State> availableActions = new Dictionary<Action, State>();
         // Go through all the actions
         foreach(Action action in Actions) {
+            Action newAction = new Action(action);
             //Debug.Log(action.Name);
             // The resulting state
             State newState = new State(state);
             bool flag = false;
             // For each requirement...
             foreach(string requirement in action.Requires) {
-                // Mark flag if doesn't meet requirement
-                if(!state.ContainsKey(requirement)) {
+                if(requirement.StartsWith("#")) { // Requirement is a category
+                    string choice = PickCategory(newState, Categories[requirement]);
+                    if(choice == null) {
+                        flag = true;
+                        break;
+                    }
+                    // Replace the category with my choice from the category
+                    newAction.Requires.Remove(requirement);
+                    newAction.Requires.Add(choice);
+                } else if(!state.ContainsKey(requirement)) {
                     Debug.Log("Warning: requirement "+requirement+" for action "+action.Name+" is not in Ingredients/Tools dictionaries.");
                 } else if(state[requirement] <= 0) {
                     flag = true;
@@ -175,8 +216,27 @@ public class ActionPlanning : MonoBehaviour
             }
             // For each item consumed...
             foreach(string item in action.Consumes.Keys) {
-                // Mark flag if not enough
-                if(!state.ContainsKey(item)) {
+                if(item.StartsWith("#")) { // If this is a category...
+                    // Pick a different option for each required instance (if you need 3 burger ingredients, each could be different)
+                    for(int i = 0; i < action.Consumes[item]; i++) {
+                        // Get my choice
+                        string choice = PickCategory(newState, Categories[item]);
+                        // If I can't choose any, can't do this action
+                        if(choice == null) {
+                            flag = true;
+                            break;
+                        } else { // Otherwise, add one of these to the newAction's consumes list and remove this from my newState
+                            newState[choice]--;
+                            if(!newAction.Consumes.ContainsKey(choice)) {
+                                newAction.Consumes.Add(choice, 1);
+                            } else {
+                                newAction.Consumes[choice]++;
+                            }
+                        }
+                    }
+                    // Remove the category now that I have added my choices from it
+                    newAction.Consumes.Remove(item);
+                } else if(!state.ContainsKey(item)) {
                     Debug.Log("Warning: item "+item+" for action "+action.Name+" is not in Ingredients/Tools dictionaries.");
                 } else if(state[item] < action.Consumes[item]) {
                     flag = true;
@@ -199,23 +259,56 @@ public class ActionPlanning : MonoBehaviour
             }
 
             // Add this action to available actions
-            availableActions.Add(action, newState);
+            availableActions.Add(newAction, newState);
         }
 
         return availableActions;
     }
 
+    string PickCategory(State state, Category category) {
+        // Pool to choose ingredient from
+        List<string> pool = new List<string>();
+        // Go through each option
+        foreach(string option in category.Keys) {
+            // If this is another category,
+            if(option[0] == '#') {
+                // Try to pick an option from it
+                string newOption = PickCategory(state, Categories[option]);
+                // If one was picked,
+                if(newOption != null) {
+                    // Add it
+                    for(int i = 0; i < category[option]; i++) {
+                        pool.Add(newOption);
+                    }
+                }
+            } else {
+                if(state[option] > 0) { // If an ingredient that I have enough of,
+                    // Add it
+                    for(int i = 0; i < category[option]; i++) {
+                        pool.Add(option);
+                    }
+                }
+
+            }
+        }
+        if(pool.Count > 0) {
+            return pool[Random.Range(0, pool.Count)];
+        } else {
+            return null;
+        }
+    }
+
     // SortedList wrapper that acts like a simple priority queue
     class PriorityQueue {
-        protected List<System.Tuple<State, int>> list;
+        protected List<System.Tuple<State, float>> list;
 
         public PriorityQueue() {
-            list = new List<System.Tuple<State, int>>();
+            list = new List<System.Tuple<State, float>>();
         }
 
-        public void Enqueue(int priority, State state) {
-            list.Add(new System.Tuple<State, int>(state, priority));
-            list.Sort((System.Tuple<State, int> x, System.Tuple<State, int> y) => { return x.Item2 - y.Item2; });
+        public void Enqueue(float priority, State state) {
+            list.Add(new System.Tuple<State, float>(state, priority));
+            list.Sort((System.Tuple<State, float> x, System.Tuple<State, float> y) => { if(x.Item2 - y.Item2 < 0) return -1; else if(x.Item2 - y.Item2 > 0) return 1; else return 0; });
         }
 
         public State Dequeue() {
@@ -252,6 +345,9 @@ public class ActionPlanning : MonoBehaviour
             }
             foreach(string tool in Tools.Keys) {
                 this.Add(tool, 0);
+            }
+            foreach(string category in Categories.Keys) {
+                this.Add(category, 0);
             }
         }
         public State(State other) {
